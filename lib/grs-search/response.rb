569b4b80cb8c3dfe05a96979cddb445aaa7a51bd
Tobias Begalke <tb@tb.unnet>
2016-04-12 13:14:50 +0200

    Version bump to 2.0.0

--- lib/grs-search/response.rb
+++ lib/grs-search/response.rb
@@ -7,58 +7,66 @@ class GRSSearch
            
     
     def initialize(json_data)
-      json_data['objects']['object'].find_all { |data| data['type'] == 'inetnum' }.each do |i|
+      json_data['objects']['object'].find_all { |data| data['type'] =~ /inet6?num/ }.each do |i|
         @source ||= i['source']['id']
         
         # IP address range
         #
-        if item = i['attributes']['attribute'].detect { |a| a['name'] == 'inetnum' }
+        if item = i['attributes']['attribute'].detect { |a| a['name'] =~ /inet6?num/ }
           _ip_from, _ip_to = item['value'].split(/\s+-\s+/).map { |ip| ip.strip }
-          @ip_from ||= IPAddr.new _ip_from
-          @ip_to ||= IPAddr.new _ip_to
+          
+          if ! _ip_to
+            _ipaddr = IPAddr.new item['value'].strip
+            _range = _ipaddr.to_range
+            @ip_from = _range.first
+            @ip_to   = _range.last
+          else
+            @ip_from ||= IPAddr.new _ip_from
+            @ip_to ||= IPAddr.new _ip_to
+          end
         end
-
+        
         # net name
         #
         if item = i['attributes']['attribute'].detect { |a| a['name'] == 'netname' }
           @name ||= item['value']
         end
-
+        
         # net description
         #
         if item = i['attributes']['attribute'].detect { |a| a['name'] == 'descr' }
           @description ||= item['value']
         end
-
+        
         # country
         #
         if item = i['attributes']['attribute'].detect { |a| a['name'] == 'country' }
           @country ||= item['value']
         end
-
+        
         # status
         #
         if item = i['attributes']['attribute'].detect { |a| a['name'] == 'status' }
           @status ||= item['value']
         end
-
+        
         # organization
         #
         if item = i['attributes']['attribute'].detect { |a| a['name'] == 'org' }
           @org ||= item['value']
         end
-
+        
       end
-
+      
       json_data['objects']['object'].find_all { |data| data['type'] == 'route' }.each do |i|
-
+        
         # network CIDR notation
         #
         if item = i['attributes']['attribute'].detect { |a| a['name'] == 'route' }
           @cidr = item['value']
         end
       end
-
+      
       def inspect
         "GRSSearch::Response\##{self.object_id} - source:#{source} ip_from:#{ip_from} ip_to:#{ip_to} name:#{name} description:#{description} country:#{country} status:#{status} cidr:#{cidr} org:#{org}"
       end
--- spec/grs-search_spec.rb
+++ spec/grs-search_spec.rb
@@ -4,6 +4,7 @@ describe GRSSearch do
   let(:res_afrinic) { GRSSearch.lookup('41.206.1.10') }
   let(:res_google) { GRSSearch.lookup('66.249.64.0') }
   let(:res_kabel) { GRSSearch.lookup('178.27.236.214') }
+	let(:res_ipv6) { GRSSearch.lookup('2804:14c:8793:89b5:add7:e834:9d29:5f75') }
 
   it "is able to fetch data from the API endpoint" do
     expect(res_google.class).to eq GRSSearch::Response
@@ -15,7 +16,7 @@ describe GRSSearch do
     expect(res_google.org).to eq 'GOGL'
     expect(res_google.name).to eq 'GOOGLE'
     expect(res_google.description).not_to be
-    expect(res_google.status).to eq 'allocation'
+    expect(res_google.status.downcase).to eq 'allocation'
     expect(res_google.country).not_to be
     expect(res_google.cidr).to eq '66.249.64.0/24'
 
@@ -43,5 +44,17 @@ describe GRSSearch do
     expect(res_kabel.country).to eq 'DE'
     expect(res_kabel.cidr).to eq '178.27.128.0/17'
 
+
+    expect(res_ipv6.class).to eq GRSSearch::Response
+    expect(res_ipv6.source).to eq 'lacnic-grs'
+    expect(res_ipv6.ip_from.to_s).to eq "2804:14c::"
+    expect(res_ipv6.ip_to.to_s).to eq "2804:14d:ffff:ffff:ffff:ffff:ffff:ffff"
+    expect(res_ipv6.ip_from.to_i).to eq 53189915322580728962426485453122174976
+    expect(res_ipv6.ip_to.to_i).to eq 53189915481037053990955160640210075647
+    expect(res_ipv6.org).not_to be
+    expect(res_ipv6.name).not_to be
+    expect(res_ipv6.status).to eq 'ALLOCATED'
+    expect(res_ipv6.country).to eq 'BR'
+    expect(res_ipv6.cidr).to eq 'CLATO S.A.'
   end
 end
